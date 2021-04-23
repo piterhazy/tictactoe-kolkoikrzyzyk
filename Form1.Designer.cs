ba70a29ef6ef91ede4b37c0e1a499d2a153a8efa
hazy <piterhazy@gmail.com>
2021-04-23 15:05:10 +0200

    izi

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace KołkoKrzyzyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nowaGraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nowaGraToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaGraToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(127, 26);
+            // 
+            // nowaGraToolStripMenuItem
+            // 
+            this.nowaGraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem});
+            this.nowaGraToolStripMenuItem.Name = "nowaGraToolStripMenuItem";
+            this.nowaGraToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.nowaGraToolStripMenuItem.Text = "Nowa Gra";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaGraToolStripMenuItem1});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(127, 26);
+            // 
+            // nowaGraToolStripMenuItem1
+            // 
+            this.nowaGraToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem1});
+            this.nowaGraToolStripMenuItem1.Name = "nowaGraToolStripMenuItem1";
+            this.nowaGraToolStripMenuItem1.Size = new System.Drawing.Size(126, 22);
+            this.nowaGraToolStripMenuItem1.Text = "Nowa Gra";
+            // 
+            // infoToolStripMenuItem1
+            // 
+            this.infoToolStripMenuItem1.Name = "infoToolStripMenuItem1";
+            this.infoToolStripMenuItem1.Size = new System.Drawing.Size(95, 22);
+            this.infoToolStripMenuItem1.Text = "Info";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(718, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.14763F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.09749F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.6156F));
+            this.tableLayoutPanel1.Controls.Add(this.b9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 48);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.53689F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.00785F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.29827F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(718, 613);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // b9
+            // 
+            this.b9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b9.FlatAppearance.BorderSize = 7;
+            this.b9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b9.Location = new System.Drawing.Point(493, 429);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(222, 181);
+            this.b9.TabIndex = 8;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b8
+            // 
+            this.b8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b8.FlatAppearance.BorderSize = 7;
+            this.b8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b8.Location = new System.Drawing.Point(241, 429);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(246, 181);
+            this.b8.TabIndex = 7;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b7
+            // 
+            this.b7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b7.FlatAppearance.BorderSize = 7;
+            this.b7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b7.Location = new System.Drawing.Point(3, 429);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(232, 181);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b6
+            // 
+            this.b6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b6.FlatAppearance.BorderSize = 7;
+            this.b6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b6.Location = new System.Drawing.Point(493, 215);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(222, 208);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b5
+            // 
+            this.b5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b5.FlatAppearance.BorderSize = 7;
+            this.b5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b5.Location = new System.Drawing.Point(241, 215);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(246, 208);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b4
+            // 
+            this.b4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b4.FlatAppearance.BorderSize = 7;
+            this.b4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b4.Location = new System.Drawing.Point(3, 215);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(232, 208);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b3
+            // 
+            this.b3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b3.FlatAppearance.BorderSize = 7;
+            this.b3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b3.Location = new System.Drawing.Point(493, 3);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(222, 206);
+            this.b3.TabIndex = 2;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b2
+            // 
+            this.b2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b2.FlatAppearance.BorderSize = 7;
+            this.b2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b2.Location = new System.Drawing.Point(241, 3);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(246, 206);
+            this.b2.TabIndex = 1;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b1
+            // 
+            this.b1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b1.FlatAppearance.BorderSize = 7;
+            this.b1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b1.Location = new System.Drawing.Point(3, 3);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(232, 206);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(718, 24);
+            this.menuStrip2.TabIndex = 4;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 661);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.MinimumSize = new System.Drawing.Size(350, 500);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kółko Krzyżyk";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nowaGraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem nowaGraToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KołkoKrzyzyk
+{
+    public partial class Form1 : Form
+    {
+        char who = 'x';
+        short movement = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nowaGraToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void infoToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button bt = sender as Button;
+            bt.Enabled = false;
+            bt.BackColor = Color.YellowGreen;
+            if (who == 'x')
+            {
+                bt.Text = "x";
+                if((b1.Text == b2.Text && b2.Text == b3.Text && b2.Text != "") ||
+                   (b4.Text == b5.Text && b5.Text == b6.Text && b5.Text != "") ||
+                   (b7.Text == b8.Text && b8.Text == b9.Text && b8.Text != "") ||
+                   (b1.Text == b4.Text && b4.Text == b7.Text && b4.Text != "") ||
+                   (b2.Text == b5.Text && b5.Text == b8.Text && b5.Text != "") ||
+                   (b3.Text == b6.Text && b6.Text == b9.Text && b6.Text != "") ||
+                   (b1.Text == b5.Text && b5.Text == b9.Text && b5.Text != "") ||
+                   (b3.Text == b5.Text && b5.Text == b7.Text && b5.Text != ""))
+                {
+                    MessageBox.Show(($"Wygrywa {who.ToString().ToUpper() } !!!"));
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if (movement == 8)
+                {
+                    MessageBox.Show("Remis!");
+                }
+                who = 'o';
+
+            }
+            else if(who == 'o')
+            {
+                bt.Text = "o";
+                if ((b1.Text == b2.Text && b2.Text == b3.Text && b2.Text != "") ||
+                   (b4.Text == b5.Text && b5.Text == b6.Text && b5.Text != "") ||
+                   (b7.Text == b8.Text && b8.Text == b9.Text && b8.Text != "") ||
+                   (b1.Text == b4.Text && b4.Text == b7.Text && b4.Text != "") ||
+                   (b2.Text == b5.Text && b5.Text == b8.Text && b5.Text != "") ||
+                   (b3.Text == b6.Text && b6.Text == b9.Text && b6.Text != "") ||
+                   (b1.Text == b5.Text && b5.Text == b9.Text && b5.Text != "") ||
+                   (b3.Text == b5.Text && b5.Text == b7.Text && b5.Text != ""))
+                {
+                    MessageBox.Show(($"Wygrywa {who.ToString().ToUpper() } !!!"));
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if (movement == 8)
+                {
+                    MessageBox.Show("Remis!");
+                }
+                who = 'x';
+            }
+            movement++;
+
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            who = 'x';
+            movement = 0;
+            b1.Enabled = true; b1.Text = ""; b1.BackColor = Color.White;
+            b2.Enabled = true; b2.Text = ""; b2.BackColor = Color.White;
+            b3.Enabled = true; b3.Text = ""; b3.BackColor = Color.White;
+            b4.Enabled = true; b4.Text = ""; b4.BackColor = Color.White;
+            b5.Enabled = true; b5.Text = ""; b5.BackColor = Color.White;
+            b6.Enabled = true; b6.Text = ""; b6.BackColor = Color.White;
+            b7.Enabled = true; b7.Text = ""; b7.BackColor = Color.White;
+            b8.Enabled = true; b8.Text = ""; b8.BackColor = Color.White;
+            b9.Enabled = true; b9.Text = ""; b9.BackColor = Color.White;
+            tableLayoutPanel1.Enabled = true;
+
+        }
+    }
+}
